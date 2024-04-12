912059b4e66ee17be6f344f09489be0128eac188
aakashss <aakash9757@gmail.com>
2024-04-12 18:30:14 +0530

    useing new file git shell

--- git.sh
+++ git.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
+
+# Get the current branch name
 git_branch=$(git rev-parse --abbrev-ref HEAD)
+
 if [ "$git_branch" = "dev" ]; then
+  # Build script for development environment
   sh './build.sh'
-  docker login -u aakshss -p dckr_pat_34-PWs-O163dyrKIzbzTPRuTXas
-  docker tag myreactapp:1.21.6-alpine aakahss/dev
-  docker push aakashss/dev:myreactapp:1.21.6-alpine
+  
+  # Docker login
+  docker login -u aakashss -p dckr_pat_34-PWs-O163dyrKIzbzTPRuTXas
+  
+  # Tag and push the image to the development repository
+  docker tag myreactapp:1.21.6-alpine aakashss/dev:myreactapp-1.21.6-alpine
+  docker push aakashss/dev:myreactapp-1.21.6-alpine
 else
-  echo "not dev repo"
+  echo "Not on dev branch, skipping development environment setup."
 fi
 
+# Switch to the prod branch
 git checkout prod
+
+# Fetch latest changes from remote
 git fetch
 
+# Check if 'dev' branch is an ancestor of 'prod'
 if git merge-base --is-ancestor dev HEAD; then
+  # Deployment script for production environment
   sh './deploy.sh'
+  
+  # Docker login
   docker login -u aakashss -p dckr_pat_34-PWs-O163dyrKIzbzTPRuTXas
-  docker tag myreactapp:1.21.6-alpine aakashss/prod
-  docker push aakahsss/prod:myreactapp:1.21.6-alpine
+  
+  # Tag and push the image to the production repository
+  docker tag myreactapp:1.21.6-alpine aakashss/prod:myreactapp-1.21.6-alpine
+  docker push aakashss/prod:myreactapp-1.21.6-alpine
 else
-  echo "dev not merged to prod"
+  echo "dev not merged to prod, skipping production deployment."
 fi
+
